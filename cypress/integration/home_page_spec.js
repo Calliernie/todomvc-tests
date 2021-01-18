cd3377ab67a9b4a2d1f586566672fc74b98f3cb4
Ernest Amuzu <ernestamuzu@ymail.com>
2021-01-18 16:37:46 +0000

    third commit

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -12,8 +12,26 @@ describe("The Home Page", () => {
   beforeEach(() => {
     //reset and ssed the database prior to every test
     cy.exec("npm rurn db:reset && npm run db:seed");
+
+    //seed a post in the DB that we can control from our tests
+
+    cy.request("POST", "/test/seed/post", {
+      title: "First Post",
+      authorId: 1,
+      body: "...",
+    });
+
+    //seed a user in the DB that we can control from our tests
+    cy.request("POST", "/test/seed/user", { name: "Jane" })
+      .its("body")
+      .as("currentUser");
   });
+
   it("successfully loads", () => {
+    //this.currentUser will now point to the response
+    //body of the cy.request() that we could use
+    //to log in or work in some way
+
     cy.visit("/");
   });
 });
